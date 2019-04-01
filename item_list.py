aa78b7a5d182952ad758969daa29971d46b66cb8
Chris Melnyk <xplowersu@gmail.com>
2019-03-31 23:38:25 -0400

    Create item_list.py

---
+++ item_list.py
@@ -0,0 +1,5 @@
+#INSERT ITEM LIST IN A GRID FORMAT
+
+
+#ITEM ICON | ITEM NAME | SMELTED | ATHMOSPHERIC CONDENSOR | USED IN 
+#                                                          (might not do this, cause compound and resin is used in a lot of stuff)

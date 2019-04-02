c0f2464f3fdb3ebf59f07aea6495fd680454141d
Chris Melnyk <xplowersu@gmail.com>
2019-04-02 11:35:46 -0400

    Update tk_app.py

--- tk_app.py
+++ tk_app.py
@@ -5,7 +5,7 @@ from PIL import ImageTk,Image
 
 planets = 'planets.py'
 item_list = 'item_list.py'
-soil_calc = 'soil/interface.py'
+soil_calc = 'soil/soil_interface.py'
 intake = ''
 
 def callpy1():
@@ -26,14 +26,14 @@ icon_img = 'images/icon_astro.jpg'
 icon = ImageTk.PhotoImage(Image.open(icon_img))
 root.tk.call('wm','iconphoto', root._w, icon)
 
-root.geometry('400x400')
+root.geometry('200x400')
 root.title('Astroneer Wiki')
 l1 = tk.Label(text="Astroneer Wiki", fg="black", bg="white")
 
-b1 = tk.Button(root, text='Planets', command=callpy1, height=6, width=57).grid(row=0, column=1)
-b2 = tk.Button(root, text='Item List', command=callpy2, height=6, width=57).grid(row=1, column=1)
-b3 = tk.Button(root, text='', command=callpy3, height=6, width=57).grid(row=2, column=1)
-b4 = tk.Button(root, text='', command=callpy4, height=6, width=57).grid(row=3, column=1)
+b1 = tk.Button(root, text='Planets', command=callpy1, height=6, width=28).grid(row=0, column=1)
+b2 = tk.Button(root, text='Item List', command=callpy2, height=6, width=28).grid(row=1, column=1)
+b3 = tk.Button(root, text='Soil Centrifuge Calculator', command=callpy3, height=6, width=28).grid(row=2, column=1)
+b4 = tk.Button(root, text='', command=callpy4, height=6, width=28).grid(row=3, column=1)
 
 
 root.mainloop()
